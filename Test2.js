a6455a7f61497cacf19423eb942c0a0138f6d49a
surgerer <surge_all@bk.ru>
2013-10-18 05:06:19 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -10,7 +10,7 @@
   function initJQ(){
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
-     newscript.async = true;
+     newscript.async = false;
      newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
   document.getElementsByTagName('head')[0].appendChild(newscript);
   }
