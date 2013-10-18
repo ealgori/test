b2632480f05ae454fae605b9f2fc28473566cfc8
surgerer <surge_all@bk.ru>
2013-10-18 04:48:28 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -1,4 +1,11 @@
-$(document).ready(function () {
+
+ 
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+)();
   var el = document; // shortcut
   var cssId = 'myCss';  // you could encode the css path itself to generate id..
   if (!el.getElementById(cssId))
@@ -17,4 +24,4 @@ $(document).ready(function () {
       $('#iframe').contents().find('input').addClass('form-control');    
   
 
-  })
+
