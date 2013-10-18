0ba8031e4c70c956ed3150b65877c12088aebf58
surgerer <surge_all@bk.ru>
2013-10-18 04:37:44 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -1,28 +1,20 @@
+jQuery(document).ready(function ($) {
+  var $ = document; // shortcut
+  var cssId = 'myCss';  // you could encode the css path itself to generate id..
+  if (!$.getElementById(cssId))
+  {
+      var head  = $.getElementsByTagName('head')[0];
+      var link  = $.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = 'https://rawgithub.com/surgerer/test/master/bootstrap.css';
+      link.media = 'all';
+      head.appendChild(link);
+  }
+  
+  
+      $('#iframe').contents().find('input').addClass('form-control');    
+  
 
-//Load jQuery library using plain JavaScript
-(function(){
-  var newscript = document.createElement('script');
-     newscript.type = 'text/javascript';
-     newscript.async = true;
-     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
-  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-})();
-
-var $ = document; // shortcut
-var cssId = 'myCss';  // you could encode the css path itself to generate id..
-if (!$.getElementById(cssId))
-{
-    var head  = $.getElementsByTagName('head')[0];
-    var link  = $.createElement('link');
-    link.id   = cssId;
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://rawgithub.com/surgerer/test/master/bootstrap.css';
-    link.media = 'all';
-    head.appendChild(link);
-}
-
-$(function(){
-    $('#iframe').contents().find('input').addClass('form-control');    
-})
-
+  }
