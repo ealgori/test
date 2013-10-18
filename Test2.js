a9a7abfda55b50d12d6237c028da1f05385f9050
surgerer <surge_all@bk.ru>
2013-10-18 04:38:37 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function ($) {
-  var $ = document; // shortcut
+  var el = document; // shortcut
   var cssId = 'myCss';  // you could encode the css path itself to generate id..
-  if (!$.getElementById(cssId))
+  if (!el.getElementById(cssId))
   {
-      var head  = $.getElementsByTagName('head')[0];
-      var link  = $.createElement('link');
+      var head  = el.getElementsByTagName('head')[0];
+      var link  = el.createElement('link');
       link.id   = cssId;
       link.rel  = 'stylesheet';
       link.type = 'text/css';
