1cb59a34b2d587031d8acdd312b206638a60a108
surgerer <surge_all@bk.ru>
2013-10-18 04:09:10 -0700

    Create Test2.js

---
+++ Test2.js
@@ -0,0 +1,7 @@
+(function(){
+  var newscript = document.createElement('script');
+     newscript.type = 'text/javascript';
+     newscript.async = true;
+     newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
+  (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
+})();
