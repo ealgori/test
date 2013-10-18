5c9527b19ada454fca62396d799ba80104eeeb39
surgerer <surge_all@bk.ru>
2013-10-18 04:49:54 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -1,11 +1,10 @@
-
  
   var newscript = document.createElement('script');
      newscript.type = 'text/javascript';
      newscript.async = true;
      newscript.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js';
   (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(newscript);
-)();
+
   var el = document; // shortcut
   var cssId = 'myCss';  // you could encode the css path itself to generate id..
   if (!el.getElementById(cssId))
