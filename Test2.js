7a4a2fae2fd210029fcb47862a0c3a4324ce4089
surgerer <surge_all@bk.ru>
2013-10-19 09:51:39 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -52,21 +52,21 @@ function iFrameExist(){
   // document.getElementsByTagName('head')[0].appendChild(newscript);
   // }
   
-  function initBootstrap(){
-  var el = document; // shortcut
-  var cssId = 'myCss';  // you could encode the css path itself to generate id..
-  if (!el.getElementById(cssId))
-  {
-      var head  = el.getElementsByTagName('head')[0];
-      var link  = el.createElement('link');
-      link.id   = cssId;
-      link.rel  = 'stylesheet';
-      link.type = 'text/css';
-      link.href = 'https://rawgithub.com/surgerer/test/master/bootstrap.css';
-      link.media = 'all';
-      head.appendChild(link);
-  }
-  }
+  // function initBootstrap(){
+  // var el = document; // shortcut
+  // var cssId = 'myCss';  // you could encode the css path itself to generate id..
+  // if (!el.getElementById(cssId))
+  // {
+  //     var head  = el.getElementsByTagName('head')[0];
+  //     var link  = el.createElement('link');
+  //     link.id   = cssId;
+  //     link.rel  = 'stylesheet';
+  //     link.type = 'text/css';
+  //     link.href = 'https://rawgithub.com/surgerer/test/master/bootstrap.css';
+  //     link.media = 'all';
+  //     head.appendChild(link);
+  // }
+  // }
   
   function AddFormControl(){
       $('#iframe').contents().find('input').addClass('form-control');    
