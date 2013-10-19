d144fb2a9aa3a595875f438ead3a7a252edd894a
surgerer <surge_all@bk.ru>
2013-10-19 09:18:34 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -18,6 +18,7 @@ function runYourFunctionWhenJQueryIsLoaded() {
 function yourFunctionToRun()
 {
   alert('jquery loaded');
+  $('*').hide();
 }
  // function initJQ(){
  // var newscript = document.createElement('script');
