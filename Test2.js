6e2aa4a7d3eaad052cee6ca720eb2fe7d7a876af
surgerer <surge_all@bk.ru>
2013-10-20 00:54:32 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -41,12 +41,12 @@ function jqLoaded()
 }
 
 
-// function unloadCss(cssName)
-// {
-//   $("link[href*="+cssName+""]").attr("disabled", "disabled");
-  
-//   console.log(url +'unloaded');
-// }
+function unloadCss(cssName)
+{
+  $("link[href*="+cssName+""]").attr("disabled", "disabled");
+ //$("link[href*=webforms]").remove(); 
+  console.log(url +'unloaded');
+}
 // function iFrameExist(){
 //   console.log('iframe exist');
   
@@ -91,27 +91,12 @@ function jqLoaded()
   }
   
   
-  // function initBootstrap(){
-  // var el = document; // shortcut
-  // var cssId = 'myCss';  // you could encode the css path itself to generate id..
-  // if (!el.getElementById(cssId))
-  // {
-  //     var head  = el.getElementsByTagName('head')[0];
-  //     var link  = el.createElement('link');
-  //     link.id   = cssId;
-  //     link.rel  = 'stylesheet';
-  //     link.type = 'text/css';
-  //     link.href = 'https://rawgithub.com/surgerer/test/master/bootstrap.css';
-  //     link.media = 'all';
-  //     head.appendChild(link);
-  //     console.log('bootstrap loaded');
-  // }
-  
-  // }
+ 
   
   function AddFormControl(){
      //$( "input" ).removeAttr('background');
-     $("link[href*=webforms]").remove();
+     //$("link[href*=webforms]").remove();
+     unloadCSS('webforms');
      $("[type='button']").addClass("btn");
      $("[name='CreateNewObjectButton']").addClass("btn-success");
      $("[name='DeleteObjectButton']").addClass("btn-danger");
