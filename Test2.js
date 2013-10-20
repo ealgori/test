bc5100d650d43770bb2ec25d0b32f58a55589f53
surgerer <surge_all@bk.ru>
2013-10-20 07:24:45 -0700

    Update Test2.js

--- Test2.js
+++ Test2.js
@@ -3,7 +3,7 @@
  
  function init(){
  
-  console.log("version 0.37");
+  console.log("version 0.38");
   waitJqLoaded();
  
  }
@@ -88,6 +88,7 @@ function unloadCSS(cssName)
       $("#formDiv").find("select").addClass('form-control');
       $("#formDiv").find("[type='text']").addClass('form-control');
       $("#formDiv > table>tbody>tr>td>div>table>tbody>tr>td>table:eq(0)").addClass('table table-striped table-hover');
+      $("#formDiv > table>tbody>tr>td>div>table>tbody>tr>td>table>td").addClass('form-inline');
       $("#dataTable>tbody>tr>td>table").css('width','57%');
      
         
