4dd88b3540a84688563048a9f20e664157e9687e
surgerer <surge_all@bk.ru>
2013-11-11 04:50:36 -0800

    Create LargeWidth.js

---
+++ LargeWidth.js
@@ -0,0 +1,97 @@
+ init();
+  
+ 
+ function init(){
+ 
+  console.log("version 0.01");
+  waitJqLoaded();
+ 
+ }
+ 
+function waitJqLoaded() {
+    if (window.$){
+        //possibly some other JQuery checks to make sure that everything is loaded here
+
+        jqLoaded();
+    } else {
+        setTimeout(waitJqLoaded, 50);
+    }
+}
+
+
+
+
+
+function jqLoaded()
+{
+  console.log('jquery loaded');
+  $( document ).ready(function() {
+  // Handler for .ready() called.
+   console.log('document ready');
+  //loadCSS('https://rawgithub.com/surgerer/test/master/table_styles.css');
+  //loadSCRIPT('https://ajax.aspnetcdn.com/ajax/jquery.validate/1.11.0/jquery.validate.min.js');
+
+  
+  AddFormControl();
+  console.log("------------");
+  })
+}
+
+
+function unloadCSS(cssName)
+{
+  $("link[href*="+cssName+"]").attr("disabled", "disabled");
+ //$("link[href*=webforms]").remove(); 
+  console.log(cssName +'unloaded');
+}
+
+  
+    function loadSCRIPT(url){
+  var newscript = document.createElement('script');
+    newscript.type = 'text/javascript';
+    newscript.async = false;
+    newscript.src = url;
+    document.getElementsByTagName('head')[0].appendChild(newscript);
+     console.log(url+' loaded');
+  }
+  
+  function loadCSS(url)
+  {
+    var el = document; // shortcut
+  var cssId = 'myCss';  // you could encode the css path itself to generate id..
+  if (!el.getElementById(cssId))
+  {
+      var head  = el.getElementsByTagName('head')[0];
+      var link  = el.createElement('link');
+      link.id   = cssId;
+      link.rel  = 'stylesheet';
+      link.type = 'text/css';
+      link.href = url;
+      link.media = 'all';
+      head.appendChild(link);
+      console.log(url+' loaded');
+  }
+  }
+  
+  function SubmitForm(wind)
+  {
+  	 wind.keepFilter();
+			 if (wind.checkSaveAsNew()) 
+			 	wind.submitAction('SaveObjectButton',"server");
+  }
+ 
+  
+  function AddFormControl(){
+  
+	//var wind = document.getElementById('iframe').contentWindow;
+	if(window.width)
+	{
+  	var wind = window;
+  	wind.$('div[id=formDiv]').css('width','1200px');
+	}
+	else
+	  console.log('no width');
+  
+ 
+
+}
