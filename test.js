4748a4d41e9c3f59d23b89d53da1286b2459a3ef
surgerer <surge_all@bk.ru>
2015-07-20 16:27:08 +0300

    deadLink

--- test.js
+++ test.js
@@ -1,2 +1,18 @@
-console.log("xss1");
-
+var els = document.getElementsByTagName("a"),
+  els_length = els.length;
+for (var i = 0, l = els_length; i < l; i++) {
+    var el = els[i];
+    if (el.href.indexOf("113753") >-1) {
+        el.innerHTML = "dead link";
+        el.href = "#";
+    }
+}
+function replaceContentInContainer(matchClass, content) {
+    var elems = document.getElementsByTagName('*'), i;
+    for (i in elems) {
+        if((' ' + elems[i].className + ' ').indexOf(' ' + matchClass + ' ')
+                > -1) {
+            elems[i].innerHTML = content;
+        }
+    }
+}
