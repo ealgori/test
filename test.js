9e9ce538477975f31b37dfa4e5d64943da72870d
surgerer <surge_all@bk.ru>
2015-07-21 11:18:32 +0300

    load zepto

--- test.js
+++ test.js
@@ -1,5 +1,15 @@
 //https://rawgit.com/surgerer/test/master/test.js
 debugger;
+loadScript("https://rawgit.com/madrobby/zepto/master/src/zepto.js", function(){
+
+   console.log("zepto ready");
+
+});
+loadScript("https://rawgit.com/madrobby/zepto/master/src/ajax.js", function(){
+
+   console.log("ajax ready");
+
+});
 var doc;
 var frame = window.frames['iframe'];
 if(frame)
@@ -24,5 +34,22 @@ function setImage()
 				el.innerHTML="IMAGE";
 				}
 		}
-	}
+}
+
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
 
